9226d49a56ec0294e2c7807e94d81e75fc23b232
johannes-code <johannes24sfj@gmail.com>
2024-12-12 11:19:33 +0100

    changed script, and images is now working

--- js/jsmaster.js
+++ js/jsmaster.js
@@ -1,11 +1,15 @@
+function display_image(){
 const selectElement = document.getElementById('picture_select');
 const img = document.getElementById('display_image')
-selectElement.addEventListener('change', function() {
-    const selectedOption = this.options
-    const imagePath = this.selectedOption;
-    console.log(imagePath)
-   console.log(selectedOption)
-if (imagePath) {
-    img.src = `images/${imagePath}.jpg`;
+
+selectElement.addEventListener('change', function () {
+    img.src = selectElement.value;
+});
 }
-}); 
+
+display_image();
+
+
+
+
+
